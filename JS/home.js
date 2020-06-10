d221d9b78806ec645ade08526bda33fd7c3f013b
avi-01 <avnishmay@gmail.com>
2020-06-10 07:41:33 +0530

    added quote to home page

--- JS/home.js
+++ JS/home.js
@@ -1,22 +1,76 @@
 
 console.log("home")
 
-var home = document.getElementById('home');
-var bigCircle = document.getElementById('bigCircle');
+const home = document.getElementById('home');
+const bigCircle = document.getElementById('bigCircle');
+const quoteDiv = document.getElementById('quote');
+const _CURSOR = document.getElementById('cursor');
 
-var pageWidth = window.innerWidth;
-var pageHeight = window.innerHeight;
-var offsetRatio = 180/(pageWidth);
+
+const pageWidth = window.innerWidth;
+const pageHeight = window.innerHeight;
+const offsetRatio = 180/(pageWidth);
+const quotes = ['Eat...','Sleep...','Code...'];
+
+var _PART = 0;
+var _PART_INDEX = 0;
+var _INTERVAL_VAL;
+
 
 window.addEventListener("mousemove", (e) => {
 
 
-    console.log("A " + pageWidth + " "  +  pageHeight + " " +offsetRatio + " " + (pageWidth/2 - e.pageX + pageHeight/2 - e.pageY))
+    // console.log("A " + pageWidth + " "  +  pageHeight + " " +offsetRatio + " " + (pageWidth/2 - e.pageX + pageHeight/2 - e.pageY))
 
     var initialRotate =(90 + (pageWidth/2 - e.pageX)*offsetRatio);
 
-    console.log(e.pageX ,e.pageY,initialRotate);
+    // console.log(e.pageX ,e.pageY,initialRotate);
 
     bigCircle.style.transform = "rotateZ("+ initialRotate +"deg)";
 
 })
+
+function Type() { 
+	var text =  quotes[_PART].substring(0, _PART_INDEX + 1);
+	quoteDiv.innerHTML = text;
+	_PART_INDEX++;
+
+	if(text === quotes[_PART]) {
+		_CURSOR.style.display = 'none';
+
+		clearInterval(_INTERVAL_VAL);
+		setTimeout(function() {
+			_INTERVAL_VAL = setInterval(Delete, 50);
+		}, 1000);
+	}
+}
+
+function Delete() {
+	var text =  quotes[_PART].substring(0, _PART_INDEX - 1);
+	quoteDiv.innerHTML = text;
+	_PART_INDEX--;
+
+	if(text === '') {
+		clearInterval(_INTERVAL_VAL);
+
+		if(_PART == (quotes.length - 1))
+			_PART = 0;
+		else
+			_PART++;
+		
+		_PART_INDEX = 0;
+
+		setTimeout(function() {
+			_CURSOR.style.display = 'inline-block';
+			_INTERVAL_VAL = setInterval(Type, 100);
+		}, 200);
+	}
+}
+
+
+const quoteAnimation = function () {
+    setTimeout(function(){_INTERVAL_VAL = setInterval(Type, 100);},3000);
+}
+
+
+quoteAnimation();
